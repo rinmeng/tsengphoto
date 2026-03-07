8aee9ac2b5c20656c3e09194bfabf1ce610b0f0f
rinmeng-gitbash <phurinmeng@gmail.com>
2026-03-06 19:17:09 -0800

    refactor: update comment for clarity in getDelayClass function

--- utils/animations.ts
+++ utils/animations.ts
@@ -1,4 +1,5 @@
 export const getDelayClass = (index: number, step = 50) => {
+  // actual delay is in global.css, this is just a helper to generate the correct class name based on index and step
   const ms = index * step;
   return ms === 0 ? 'delay-0' : `delay-[${ms}ms]`;
 };
