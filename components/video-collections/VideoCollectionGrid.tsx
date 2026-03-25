5c1283524cb6b799736eb990b32c74df79932754
rinmeng-gitbash <phurinmeng@gmail.com>
2026-03-24 23:26:39 -0700

    fix: update grid layout to use 'xl' breakpoint for VideoCollectionGrid component

--- components/video-collections/VideoCollectionGrid.tsx
+++ components/video-collections/VideoCollectionGrid.tsx
@@ -35,7 +35,7 @@ export function VideoCollectionGrid({
   }
 
   return (
-    <div className='container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+    <div className='container grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6'>
       {videoCollections.map((videoCollection, index) => (
         <VideoCollectionCard
           className={`h-full fade-in-from-bottom ${getDelayClass(index)}`}
