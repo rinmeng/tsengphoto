0ff1bc990f004bbfbf5eafa44cc38dc9a14a9f51
rinmeng-gitbash <phurinmeng@gmail.com>
2026-04-06 22:37:33 -0700

    feat: update AboutPage content and optimize about image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { Text } from '@/components/Text';
 import { OptimizedImage } from '@/components/OptimizedImage';
+import { Text } from '@/components/Text';
 import { Separator } from '@/components/ui';
 import { getDelayClass } from '@/utils/animations';
 
@@ -15,8 +15,6 @@ export default function AboutPage() {
       <Separator className='border-t-2' />
       <div className={'container mx-auto border-x-2 border-dashed p-4'}>
         <div className='max-w-4xl mx-auto space-y-8'>
-          {/* Header */}
-          {/* Image */}
           <div
             className={`relative w-full aspect-video rounded overflow-hidden
               fade-in-from-top ${getDelayClass(1)}`}
@@ -32,11 +30,14 @@ export default function AboutPage() {
           {/* Bio Text */}
           <div className={`space-y-4 fade-in-from-top ${getDelayClass(2)}`}>
             <Text variant='bd-lg'>
-              Hello! I&apos;m Matthew Tseng and I have been an event photographer for
-              clubs on the UBCO campus for 2 years. My vision of a perfect shot is one
-              that prioritizes the main subject while keeping colours more natural-like.
-              My goal is to bring satisfaction to people through the images I take of
-              them!
+              Hello! My name is Matthew Tseng and I&apos;m an event photographer with 4+
+              years of experience capturing both photos and videos. My photography journey
+              began in 2016 when I was a highschool student. Starting with portraits and
+              landscape, before transitioning to event photography when I got involved in
+              university clubs and activities. My vision of a perfect shot is one that
+              prioritizes the main subject while keeping colours more natural-like. My
+              goal is to bring satisfaction to people through the images I take of them! I
+              look forward to working with you
             </Text>
             <Text variant='bd-lg' className={`fade-in-from-top ${getDelayClass(3)}`}>
               I look forward to working with you!
